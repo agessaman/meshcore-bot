2be93ebf6f0c95c35282efd90cac0e5b7e59f6ad
Tilman Baumann <tilman@baumann.name>
2026-01-07 15:15:42 +0100

    Allowign to pass --config to web viewer

--- modules/web_viewer/app.py
+++ modules/web_viewer/app.py
@@ -3899,10 +3899,12 @@ def main():
     parser.add_argument('--host', default='127.0.0.1', help='Host to bind to')
     parser.add_argument('--port', type=int, default=8080, help='Port to bind to')
     parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+    parser.add_argument( "--config", default="config.ini", help="Path to configuration file (default: config.ini)",
+    )
     
     args = parser.parse_args()
     
-    viewer = BotDataViewer()
+    viewer = BotDataViewer(config_path=args.config)
     viewer.run(host=args.host, port=args.port, debug=args.debug)
 
 if __name__ == '__main__':
